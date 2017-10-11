bb276a5f3e7bd879bfb47e05232bb5023bb80f50
nirmalwsolus <nirmal@wsolus.com>
2017-10-11 13:55:01 +0530

    Onging dev

--- Ch03/03_05/start/src/components/SkiDayCount.js
+++ Ch03/03_05/start/src/components/SkiDayCount.js
@@ -1,13 +1,16 @@
-import React from 'react'
+// import React from 'react'
+import {Component} from 'react'
 import '../stylesheets/ui.scss'
 
-export const SkiDayCount = React.createClass({
+//Commented lines are replaced
+// export class SkiDayCount extends React.Component{
+export class SkiDayCount extends Component{
 	percentToDecimal(decimal) {
 		return ((decimal * 100) + '%')
-	},
+	}
 	calcGoalProgress(total, goal) {
 		return this.percentToDecimal(total/goal)
-	},
+	}
 	render() {
 		return (
 			<div className="ski-day-count">
@@ -26,7 +29,7 @@ export const SkiDayCount = React.createClass({
 				<div>
 					<span>
 						{this.calcGoalProgress(
-							this.props.total, 
+							this.props.total,
 							this.props.goal
 						)}
 					</span>
@@ -34,4 +37,40 @@ export const SkiDayCount = React.createClass({
 			</div>
 		)
 	}
-})
+}
+
+
+// export const SkiDayCount = React.createClass({
+// 	percentToDecimal(decimal) {
+// 		return ((decimal * 100) + '%')
+// 	},
+// 	calcGoalProgress(total, goal) {
+// 		return this.percentToDecimal(total/goal)
+// 	},
+// 	render() {
+// 		return (
+// 			<div className="ski-day-count">
+// 				<div className="total-days">
+// 					<span>{this.props.total}</span>
+// 					<span>days</span>
+// 				</div>
+// 				<div className="powder-days">
+// 					<span>{this.props.powder}</span>
+// 					<span>days</span>
+// 				</div>
+// 				<div className="backcountry-days">
+// 					<span>{this.props.backcountry}</span>
+// 					<span>days</span>
+// 				</div>
+// 				<div>
+// 					<span>
+// 						{this.calcGoalProgress(
+// 							this.props.total,
+// 							this.props.goal
+// 						)}
+// 					</span>
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// })
