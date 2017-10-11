8d1ae8a39ceb259c405e5ccc659b2479490cdb2f
nirmalwsolus <nirmal@wsolus.com>
2017-10-11 16:29:10 +0530

    Ongoing dev

--- Ch03/03_07/start/src/components/SkiDayCount.js
+++ Ch03/03_07/start/src/components/SkiDayCount.js
@@ -1,4 +1,7 @@
 import '../stylesheets/ui.scss'
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
 
 const percentToDecimal = (decimal) => {
 	return ((decimal * 100) + '%')
@@ -9,26 +12,29 @@ const calcGoalProgress = (total, goal) => {
 }
 
 export const SkiDayCount = ({total, powder, backcountry, goal}) => (
-		<div className="ski-day-count">
-			<div className="total-days">
-				<span>{total}</span>
-				<span>days</span>
-			</div>
-			<div className="powder-days">
-				<span>{powder}</span>
-				<span>days</span>
-			</div>
-			<div className="backcountry-days">
-				<span>{backcountry}</span>
-				<span>days</span>
-			</div>
-			<div>
+	<div className="ski-day-count">
+		<div className="total-days">
+			<span>{total}</span>
+			<Calendar />
+			<span>days</span>
+		</div>
+		<div className="powder-days">
+			<span>{powder}</span>
+			<SnowFlake />
+			<span>days</span>
+		</div>
+		<div className="backcountry-days">
+			<span>{backcountry}</span>
+			<Terrain />
+			<span>days</span>
+		</div>
+		<div>
 				<span>
 					{calcGoalProgress(
-						total, 
+						total,
 						goal
 					)}
 				</span>
-			</div>
 		</div>
+	</div>
 )
