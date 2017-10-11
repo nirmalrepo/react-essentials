8676ddf8e7158d9d60e942b5b348260f9c5d986b
nirmalwsolus <nirmal@wsolus.com>
2017-10-11 11:53:47 +0530

    Ongoing dev

--- Ch03/03_04/start/src/components/SkiDayCount.js
+++ Ch03/03_04/start/src/components/SkiDayCount.js
@@ -2,23 +2,35 @@ import React from 'react'
 import '../stylesheets/ui.scss'
 
 export const SkiDayCount = React.createClass({
+	percentToDecimal(decimal){
+		return ((decimal * 100) + '%')
+	},
+
+	calcGoalProgress(total, goal){
+		return this.percentToDecimal(total/goal);
+	},
 	render() {
 		return (
 			<div className="ski-day-count">
 				<div className="total-days">
 					<span>{this.props.total}</span>
-					<span>days</span>
+					<span> days</span>
 				</div>
 				<div className="powder-days">
 					<span>{this.props.powder}</span>
-					<span>days</span>
+					<span> days</span>
 				</div>
 				<div className="backcountry-days">
 					<span>{this.props.backcountry}</span>
-					<span>days</span>
+					<span> hiking day</span>
 				</div>
-				<div>
-					<span>{this.props.goal}</span>
+				<div >
+					<span>
+						{this.calcGoalProgress(
+							this.props.total,
+							this.props.goal
+						)}
+					</span>
 				</div>
 			</div>
 		)
