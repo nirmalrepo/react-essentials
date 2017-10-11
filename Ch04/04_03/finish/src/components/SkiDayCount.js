056ed411e43f47cdd1a192b7cb56bb7427aa7b77
nirmalwsolus <nirmal@wsolus.com>
2017-10-11 17:39:49 +0530

    Onging dev

--- Ch04/04_03/finish/src/components/SkiDayCount.js
+++ Ch04/04_03/finish/src/components/SkiDayCount.js
@@ -38,3 +38,9 @@ export const SkiDayCount = ({total, powder, backcountry, goal}) => (
 			</div>
 		</div>
 )
+SkiDayCount.defaultProps = {
+	total: 50,
+	powder: 10,
+	backcountry: 15,
+	goal: 75
+}
