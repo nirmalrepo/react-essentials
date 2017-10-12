a3b1abfdfe9a006e3005fe160da8727aa63b6417
nirmalwsolus <nirmal@wsolus.com>
2017-10-12 12:59:03 +0530

    Onging dev

---
+++ Ch05/05_01/start/src/components/Whoops404.js
@@ -0,0 +1,7 @@
+/**
+ * Created by nirmal on 10/12/17.
+ */
+export const Whoops404 = () =>
+    <div>
+        <h1>Whoops, route not found</h1>
+    </div>
---
+++ Ch05/05_02/start/src/components/AddDayForm.js
@@ -0,0 +1,6 @@
+/**
+ * Created by nirmal on 10/12/17.
+ */
+export const AddDayForm = () => (
+    <h1>Add A Day</h1>
+)
